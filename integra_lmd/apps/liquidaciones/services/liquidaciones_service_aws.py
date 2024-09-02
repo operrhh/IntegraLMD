a9ccdb45080b2b1937b8d0d0cba4c7950ea4ee43
Luciano <lucian.pino.a@gmail.com>
2024-09-02 16:01:53 -0400

    chore: Add test_bucket method to BucketService in liquidaciones_service_aws.py

--- integra_lmd/apps/liquidaciones/services/liquidaciones_service_aws.py
+++ integra_lmd/apps/liquidaciones/services/liquidaciones_service_aws.py
@@ -1,7 +1,7 @@
 from typing import List
 from django.db import connections
 import boto3
-
+import os
 
 
 # class BucketService:
@@ -19,3 +19,24 @@ import boto3
 
 #     def put_file(self, key, file):
 #         return self.get_bucket().put_object(Key=key, Body=file)
+
+
+class BucketService:
+    def test_bucket(self):
+        try:
+            name_bucket = os.getenv('BK_AWS_BUCKET_NAME')
+
+            s3 = boto3.client('s3',aws_access_key_id = os.getenv('BK_AWS_ACCESS_KEY_ID'), aws_secret_access_key = os.getenv('BK_AWS_SECRET_ACCESS_KEY'))
+
+            res = s3.list_objects_v2(Bucket=name_bucket)
+
+            if 'Contents' in res:
+                for obj in res['Contents']:
+                    print(obj['Key'])
+                    name_file = obj['Key']
+                    #s3.download_file(name_bucket, name_file, name_file)
+
+
+            return 'test_bucket'
+        except Exception as e:
+            raise e
